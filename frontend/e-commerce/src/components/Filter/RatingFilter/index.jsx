ee77ca728b1e2a0382e1012f92e24edba3567a66
NicoleAsqui <nasqui@espol.edu.ec>
2024-01-29 19:15:40 -0500

    Fix the filters by rate

--- frontend/e-commerce/src/components/Filter/RatingFilter/index.jsx
+++ frontend/e-commerce/src/components/Filter/RatingFilter/index.jsx
@@ -1,18 +1,26 @@
-import { Rating } from './Rating'
-import './RatingFilter.css'
+import { useContext } from 'react';
+import { SearchContext } from '../../../contexts/SearchContext';
+import { Rating } from './Rating';
+import './RatingFilter.css';
 
-function RatingFilter () {
-    return (
-        <div className='RatingFilterContainer'>
-            <h2>Rates:</h2>
-            <div className='RatingsContainer'>
-                <Rating stars={4} />
-                <Rating stars={3} />
-                <Rating stars={2} />
-                <Rating stars={1} />
-            </div>
-        </div>
-    )
+function RatingFilter() {
+  const { products } = useContext(SearchContext);
+
+  const uniqueRates = [...new Set(products.map((product) => (
+    product.rating ? Math.round(product.rating.rate) : null)))].sort(
+    (a, b) => a - b
+  );
+
+  return (
+    <div className='RatingFilterContainer'>
+      <h2>Rates:</h2>
+      <div className='RatingsContainer'>
+        {uniqueRates.map((rate, index) => (
+          <Rating key={index} stars={rate} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export { RatingFilter }
+export { RatingFilter };
